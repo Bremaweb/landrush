a96edfbbbec85c3219046c37af50d0af3017522a
Brandon <brandon@bremaweb.com>
2013-08-17 22:27:13 -0500

    door blue texture, add unlocked sign

--- init.lua
+++ init.lua
@@ -363,8 +363,51 @@ end
 
 -- End of fix
 
---landrush.register_claimnode("landclaim", "landrush_landclaim.png")
---landrush.register_claimnode("landclaim_b", "landrush_landclaim.png")
+-- create a new type of sign that is not protected by landrush mod
+minetest.register_node("landrush:unlocked_sign", {
+	description = "Unprotected Sign",
+	drawtype = "signlike",
+	tiles = {"default_sign_wall.png"},
+	inventory_image = "default_sign_wall.png",
+	wield_image = "default_sign_wall.png",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	sunlight_propagates = true,
+	walkable = false,
+	selection_box = {
+		type = "wallmounted",
+		--wall_top = <default>
+		--wall_bottom = <default>
+		--wall_side = <default>
+	},
+	groups = {choppy=2,dig_immediate=2,attached_node=1},
+	legacy_wallmounted = true,
+	sounds = default.node_sound_defaults(),
+	on_construct = function(pos)
+		--local n = minetest.get_node(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("formspec", "field[text;;${text}]")
+		meta:set_string("infotext", "\"\"")
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		local meta = minetest.get_meta(pos)
+		fields.text = fields.text or ""
+		print((sender:get_player_name() or "").." wrote \""..fields.text..
+				"\" to sign at "..minetest.pos_to_string(pos))
+		meta:set_string("text", fields.text)
+		meta:set_string("infotext", '"'..fields.text..'"')
+	end,
+})
+
+
+minetest.register_craft({
+	output = 'landrush:unlocked_sign',
+	recipe = {
+		{'default:wood','default:wood','default:wood'},
+		{'default:wood','default:wood','landrush:landclaim'},
+		{'','default:stick',''}
+	}
+})
 
 minetest.register_entity("landrush:showarea",{
 	on_activate = function(self, staticdata, dtime_s)
